4897218b9989561cdbb33f4eaed369f8d28813c9
Tofiq Nasibli <tofiqnasibli@Tofiqs-MacBook-Air.local>
2024-03-17 09:11:46 +0400

    added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,30 @@
-"use strict";
-
-let basket = [];
-
-if (JSON.parse(localStorage.getItem("basket")) == null) {
-  localStorage.setItem("basket", JSON.stringify(basket));
-} else {
-  basket = JSON.parse(localStorage.getItem("basket"));
-}
-
-getBasketCount(basket);
-
-function getBasketCount(arr) {
-  let basketCount = 0;
-
-  if (arr.length != 0) {
-    for (const item of arr) {
-      basketCount += item.count;
-    }
-  }
-  document.querySelector(".navigation .basket-count").innerText = basketCount;
-}
-
-let addBtns = document.querySelectorAll("#products .add-btn");
-
-addBtns.forEach((btn) => {
-  btn.addEventListener("click", function (e) {
-    e.preventDefault();
-    let productId = parseInt(
-      this.parentNode.parentNode.getAttribute("data-id")
-    );
-    let productName = this.parentNode.firstElementChild.innerText;
-    let productDesc = this.previousElementSibling.innerText;
-    let productImage =
-      this.parentNode.previousElementSibling.getAttribute("src");
-    let productPrice = parseFloat(
-      this.nextElementSibling.firstElementChild.innerText
-    );
-
-    let existProduct = basket.find((m) => m.id == productId);
-
-    if (existProduct != undefined) {
-      existProduct.count++;
+$(document).ready(function() {
+  var currentSlide = 0;
+  var totalSlides = $('.slider-item').length;
+  
+  $('.slider-item').eq(currentSlide).show();
+  
+  $('.prev').click(function() {
+    if (currentSlide > 0) {
+      currentSlide--;
     } else {
-      basket.push({
-        id: productId,
-        name: productName,
-        description: productDesc,
-        image: productImage,
-        price: productPrice,
-        count: 1,
-      });
+      currentSlide = totalSlides - 1;
     }
+    $('.slider-item').hide().eq(currentSlide).fadeIn();
+  });
+  
+  $('.next').click(function() {
+    if (currentSlide < totalSlides - 1) {
+      currentSlide++;
+    } else {
+      currentSlide = 0;
+    }
+    $('.slider-item').hide().eq(currentSlide).fadeIn();
+  });
+});
 
-    getBasketCount(basket);
-
-    localStorage.setItem("basket", JSON.stringify(basket));
+$(document).ready(function(){
+  $('.toggle-btn').click(function(){
+      $('.sidebar').toggleClass('show');
   });
 });
